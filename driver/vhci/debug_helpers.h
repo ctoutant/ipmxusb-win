0119a07273c3961d16ccd70390498410dfea759e
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2020-03-18 17:59:53 +0100

    stringify line, and function is not there, file is fine to since we have line

--- driver/vhci/debug_helpers.h
+++ driver/vhci/debug_helpers.h
@@ -4,8 +4,11 @@
 #error dont include this on its own, it needs wdm.h already included
 #endif
 
+#define TO_STR(s) _TO_STR(s)
+#define _TO_STR(s) #s
+
 #define DBGI_FUN(part, fmt, ...) \
-    DBGI(part, __FUNCTION__ "@" __LINE__ fmt, ## __VA_ARGS__)
+    DBGI(part, __FILE__ "@" TO_STR(__LINE__) "" fmt, ## __VA_ARGS__)
 
 #undef IoCompleteRequest
 #define IoCompleteRequest(a,b) \
